78437f0c56802b25380effc4459c86ecc3b28866
PaulRwagasana <p.rwagasana@alustudent.com>
2026-08-01 20:35:57 +0200

    resolving state conflicts

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,7 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = var.environment
+    project     = "medstock-monitor"
+  }
+}
+
 module "network" {
   source              = "./modules/network"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   name_prefix         = var.name_prefix
   tags = {
     environment = var.environment
@@ -9,11 +19,11 @@ module "network" {
 }
 
 module "bastion" {
-  source              = "./modules/bastion"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  name_prefix         = var.name_prefix
-  subnet_id           = module.network.public_subnet_id
+  source               = "./modules/bastion"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  name_prefix          = var.name_prefix
+  subnet_id            = module.network.public_subnet_id
   admin_ssh_public_key = var.admin_ssh_public_key
   allowed_ssh_cidrs    = var.allowed_ssh_cidrs
   tags = {
@@ -22,12 +32,25 @@ module "bastion" {
 }
 
 module "db" {
-  source               = "./modules/db"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  name_prefix          = var.name_prefix
+  source                 = "./modules/db"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  name_prefix            = var.name_prefix
   administrator_password = var.db_administrator_password
+  subnet_id              = module.network.private_subnet_id
+  tags = {
+    environment = var.environment
+  }
+}
+
+module "compute" {
+  source               = "./modules/compute"
+  resource_group_name  = azurerm_resource_group.rg.name
+  location             = azurerm_resource_group.rg.location
+  name_prefix          = var.name_prefix
   subnet_id            = module.network.private_subnet_id
+  nsg_id               = module.network.private_nsg_id
+  admin_ssh_public_key = var.admin_ssh_public_key
   tags = {
     environment = var.environment
   }
@@ -35,24 +58,11 @@ module "db" {
 
 module "registry" {
   source              = "./modules/registry"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   name_prefix         = var.name_prefix
   admin_enabled       = var.acr_admin_enabled
   principal_id        = module.compute.identity_principal_id
-  tags = {
-    environment = var.environment
-  }
-}
-
-module "compute" {
-  source              = "./modules/compute"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  name_prefix         = var.name_prefix
-  subnet_id           = module.network.private_subnet_id
-  nsg_id              = module.network.private_nsg_id
-  admin_ssh_public_key = var.admin_ssh_public_key
   tags = {
     environment = var.environment
   }
